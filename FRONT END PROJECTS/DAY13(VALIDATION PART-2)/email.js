6d000ffdd926c5bed15c27177c5e43fc0cd26ac7
DhruviThakkar210 <103808644+DhruviThakkar210@users.noreply.github.com>
2022-09-19 17:13:34 +0530

    Add files via upload

---
+++ FRONT END PROJECTS/DAY13(VALIDATION PART-2)/email.js
@@ -0,0 +1,16 @@
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
