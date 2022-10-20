97d10d01f167dd0a483c43dc98b724c23d75e308
DhruviThakkar210 <103808644+DhruviThakkar210@users.noreply.github.com>
2022-10-20 23:03:31 +0530

    Add files via upload

---
+++ BACKEND/PHP/practical-2/swap.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "BEFORE SWAPPING ! :";
+$a = "DHRUVI";
+$b = "THAKKAR";
+
+
+
+echo $a;
+echo $b;
+ 
+
+ $c = $a;
+ $a = $b;
+ $b = $c;
+ echo"</br>";
+echo "AFTER SWAPPING ! : ";
+ echo $a;
+ echo $b;
+
+
+
+?>
